e51e014660c44a2ec3efa569d549225fa93ce094
fahkhaer <khaeranifah@gmail.com>
2025-11-15 11:01:50 +0700

    install shadcn and update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +16,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'), 
     },
   },
   css: {
