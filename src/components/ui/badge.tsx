c9d5884efc3fb07ccd13b5a8fd7a5a350ec97c14
fahkhaer <khaeranifah@gmail.com>
2025-11-18 09:54:52 +0700

    badge

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const badgeVariants = cva(
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
+        secondary:
+          'border-transparent bg-[#24A5000D] text-sm font-bold text-[#24A500] hover:bg-secondary/80',
+        destructive:
+          'border-transparent w-fit rounded-1 text-sm font-bold bg-[#EE1D521A] text-[#EE1D52] hover:bg-destructive/80',
+        outline: 'text-foreground leading-7',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
