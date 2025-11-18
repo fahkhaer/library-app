24cbed6cfb2235c2915a6023fec2d13c64b47967
fahkhaer <khaeranifah@gmail.com>
2025-11-18 09:57:42 +0700

    User Tabs

---
+++ src/pages/UserTabs.tsx
@@ -0,0 +1,31 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import BorrowedList from './BorrowedList';
+import Profile from './Profile';
+import BookList from './BookList';
+
+function UserTabs() {
+  return (
+    <section className='pt-12 pb-[45px]'>
+      {/* tabs */}
+      <Tabs defaultValue='booklist' className=' rounded-xl '>
+        <TabsList className='bg-[#F5F5F5]'>
+          <TabsTrigger value='profile'>Profile</TabsTrigger>
+          <TabsTrigger value='borrowedlist'>Borrowed List</TabsTrigger>
+          <TabsTrigger value='booklist'>Book List</TabsTrigger>
+        </TabsList>
+        {/* content */}
+        <TabsContent value='profile'>
+          <Profile />
+        </TabsContent>
+        <TabsContent value='borrowedlist'>
+          <BorrowedList />
+        </TabsContent>
+        <TabsContent value='booklist'>
+          <BookList />
+        </TabsContent>
+      </Tabs>
+    </section>
+  );
+}
+
+export default UserTabs;
