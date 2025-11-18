3839762cdd02e0234e4a8afe8b9dfd68bec6d739
fahkhaer <khaeranifah@gmail.com>
2025-11-18 09:48:28 +0700

    Navbar

---
+++ src/pages/Navbar.tsx
@@ -0,0 +1,72 @@
+import { Badge } from '@/components/ui/badge';
+import { Command, CommandInput } from '@/components/ui/command';
+import { Icon } from '@iconify/react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { ChevronDown } from 'lucide-react';
+
+function Navbar() {
+  return (
+    <div
+      className='flex font-quicksand h-20 px-[120px] justify-between shadow-[0_0_20px_0_#CBCACA40]
+items-center'
+    >
+      <div className='size-10 w-fit gap-4 flex'>
+        <img src='/logotext.png' alt='Logo-text' />
+      </div>
+      {/* search bar */}
+      <div>
+        <Command className='rounded-full justify-center h-22 border border-neutral-300 gap-2 px-4 md:min-w-[500px]'>
+          <CommandInput
+            className='text-neutral-600 text-sm '
+            placeholder='Search book '
+          />
+        </Command>
+      </div>
+
+      {/* right side icons */}
+      <div className='flex gap-6 items-center '>
+        <div className='relative flex'>
+          <Icon
+            className='relative'
+            icon='lets-icons:bag-fill'
+            width='32'
+            height='32'
+          />{' '}
+          <Badge className='absolute hover:bg-red-700 left-5 bg-[#EE1D52] text-white h-5 min-w-5 text-center  rounded-[833.33px] font-mono tabular-nums p-[6.67px]  '>
+            1
+          </Badge>
+        </div>{' '}
+        <div className='flex gap-4 items-center'>
+          <Avatar>
+            <AvatarImage src='https://github.com/shadcn.png' />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <p className='text-lg-semibold'>John Doe</p>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <ChevronDown className='' />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
