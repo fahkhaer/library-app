57b85549f707f8b40d873b0b3a99fe821ba21ecc
fahkhaer <khaeranifah@gmail.com>
2025-11-20 09:06:49 +0700

    update usertabs + add reviews page

---
+++ src/pages/Reviews.tsx
@@ -0,0 +1,50 @@
+import CardList from '@/components/ui/CardList';
+import { Command, CommandInput } from '@/components/ui/command';
+import { Star } from 'lucide-react';
+
+function Reviews() {
+  const star = 3;
+
+  return (
+    <section className='flex flex-col mt-6 gap-6 pb-[110px]'>
+      <h1>Book List</h1>
+
+      {/* search bar */}
+      <div className='w-[600px]'>
+        <Command className='h-12 rounded-full justify-center border border-neutral-300 gap-2 px-4 md:min-w-[500px]'>
+          <CommandInput
+            className='text-neutral-600 text-sm '
+            placeholder='Search book '
+          />
+        </Command>
+      </div>
+      {/* tabs and content */}
+      <div className='shadow-card divide-neutral-300 divide-y bg-white rounded-2xl p-5 space-y-5'>
+        <p className='text-md-semibold'> 25 August 2025, 13:38 </p>
+        <div className='pt-5'>
+          <CardList />
+        </div>
+        <div className='flex flex-col gap-2 pt-5'>
+          <div className='flex'>
+            {Array.from({ length: 5 }).map((_, i) => (
+              <Star
+                key={i}
+                className='inline-block size-6 border-none text-[#FFAB0D]'
+                stroke={i < star ? '#FFAB0D' : '#A4A7AE'}
+                fill={i < star ? '#FFAB0D' : '#A4A7AE'}
+              />
+            ))}
+          </div>
+          {/* comment */}
+          <p className='text-md-semibold'>
+            Lorem ipsum dolor sit amet consectetur. Pulvinar porttitor aliquam
+            viverra nunc sed facilisis. Integer tristique nullam morbi mauris
+            ante.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Reviews;
--- src/pages/UserTabs.tsx
+++ src/pages/UserTabs.tsx
@@ -1,17 +1,17 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import BorrowedList from './BorrowedList';
 import Profile from './Profile';
-import BookList from './BookList';
+import Reviews from './Reviews';
 
 function UserTabs() {
   return (
     <section className='pt-12 pb-[45px]'>
       {/* tabs */}
-      <Tabs defaultValue='profile' className=' rounded-xl '>
+      <Tabs defaultValue='borrowedlist' className=' rounded-xl '>
         <TabsList className='bg-[#F5F5F5]'>
           <TabsTrigger value='profile'>Profile</TabsTrigger>
           <TabsTrigger value='borrowedlist'>Borrowed List</TabsTrigger>
-          <TabsTrigger value='booklist'>Book List</TabsTrigger>
+          <TabsTrigger value='reviews'>Reviews</TabsTrigger>
         </TabsList>
         {/* content */}
         <TabsContent value='profile'>
@@ -20,8 +20,8 @@ function UserTabs() {
         <TabsContent value='borrowedlist'>
           <BorrowedList />
         </TabsContent>
-        <TabsContent value='booklist'>
-          <BookList />
+        <TabsContent value='reviews'>
+          <Reviews />
         </TabsContent>
       </Tabs>
     </section>
