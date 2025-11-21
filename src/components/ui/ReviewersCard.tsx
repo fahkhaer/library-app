25733ed4bc73851b937079b38db97dd739acbaa7
fahkhaer <khaeranifah@gmail.com>
2025-11-21 08:35:07 +0700

    edit reviewer card

--- src/components/ui/ReviewersCard.tsx
+++ src/components/ui/ReviewersCard.tsx
@@ -18,7 +18,7 @@ function ReviewersCard({ review }: ReviewersCardProps) {
   const star = review?.star ?? 0;
 
   return (
-    <div className='bg-white flex flex-col rounded-2xl md:w-1/2 gap-4 p-4 '>
+    <div className='bg-white flex flex-col rounded-2xl w-full md:w-1/2 gap-4 p-4 '>
       {/* profile */}
       <div className='flex gap-3 h-16 items-center '>
         <Avatar className='size-16 '>
