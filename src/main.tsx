42055e8565a4b49e081a6338cbf580965ec55529
fahkhaer <khaeranifah@gmail.com>
2025-11-21 08:31:29 +0700

    add router main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,50 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Navbar from './pages/Navbar.tsx';
-
-import Home from './pages/Home.tsx';
-import Category from './pages/Category.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import DetailPage from './pages/DetailPage.tsx';
+import Category from './pages/Category.tsx';
 import BookByAuthor from './pages/BookByAuthor.tsx';
 import Cart from './pages/Cart.tsx';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
 import UserTabs from './pages/UserTabs.tsx';
 import AdminTabs from './pages/AdminTabs.tsx';
+import Home from './pages/Home.tsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout.tsx';
+import AuthLayout from './components/layout/AuthLayout.tsx';
+import Checkout from './pages/Checkout.tsx';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.ts';
+import Success from './pages/Success.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar />
-    <div className='px-[120px] font-quicksand bg-[#fafafa] '>
-      <Register />
-      <Login />
-      <UserTabs />
-      <AdminTabs />
-      <Home />
-      <DetailPage />
-      <Category />
-      <BookByAuthor />
-      <Cart />
-    </div>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AuthLayout />}>
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+              <Route path='/success' element={<Success />} />
+            </Route>
+            <Route element={<MainLayout />}>
+              <Route path='/' element={<Home />} />
+              <Route path='/user' element={<UserTabs />} />
+              <Route path='/admin' element={<AdminTabs />} />
+              <Route path='/detail/:id' element={<DetailPage />} />
+              <Route path='/category' element={<Category />} />
+              <Route path='/authors-book/:id' element={<BookByAuthor />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/checkout' element={<Checkout />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
