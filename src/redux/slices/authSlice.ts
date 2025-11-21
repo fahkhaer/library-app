4a92e319957112da085125a3ac9eb2c9215748b6
fahkhaer <khaeranifah@gmail.com>
2025-11-21 08:29:14 +0700

    add redux slice

---
+++ src/redux/slices/authSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '@/types/auth';
+
+type AuthState = {
+  user: User | null;
+  token: string | null;
+};
+
+const initialState: AuthState = {
+  user: null,
+  token: localStorage.getItem('token'),
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+    setToken: (state, action: PayloadAction<string>) => {
+      state.token = action.payload;
+      localStorage.setItem('token', action.payload);
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem('token');
+    },
+  },
+});
+
+export const { setUser, setToken, logout } = authSlice.actions;
+export default authSlice.reducer;
