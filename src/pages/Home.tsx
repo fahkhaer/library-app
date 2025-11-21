d64a966ad8bbbdddd8fbc7fe5c06cdcca8c7b196
fahkhaer <khaeranifah@gmail.com>
2025-11-21 08:34:22 +0700

    api genres and autohr

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import AuthorCard from '@/components/ui/AuthorCard';
+import { Bookrecomendation } from '@/api/booklist';
 import Card from '@/components/ui/Card';
 import {
   Carousel,
@@ -8,24 +8,44 @@ import {
   CarouselPrevious,
 } from '@/components/ui/carousel';
 import LoadMoreButton from '@/components/ui/LoadMoreButton';
+import { Book } from '@/types/books';
+import PopularAuthor from '@/components/ui/PopularAuthor';
+import { Link } from 'react-router-dom';
+import { GetCategories } from '@/api/categories';
 
 function Home() {
-  const genres = [
-    { name: 'Fiction', icon: '/fiction.png' },
-    { name: 'Non-Fiction', icon: '/non-fiction.png' },
-    { name: 'Self-Improvement', icon: '/self-improvement.png' },
-    { name: 'Finance ', icon: '/finance.png' },
-    { name: 'Science', icon: '/science.png' },
-  ];
+
+  const {
+    data: categoriesData,
+    isLoading: isLoadingCategories,
+    error: errorCategories,
+  } = GetCategories();
+
+  if (isLoadingCategories) return <p>Loading...</p>;
+  if (errorCategories) return <p>Error...</p>;
+
+  const { data, isLoading, error } = Bookrecomendation();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error...</p>;
+
+  const genres = categoriesData?.map((item) => ({
+    name: item.name,
+    icon: item.name ? `/${item.name}.png` : 'image-off.png',
+  }));
+
   return (
     <>
       <div className=' md:pb-[116px] md:pt-12'>
-        <Carousel className=''>
+        <Carousel>
           <CarouselContent>
             {Array.from({ length: 5 }).map((_, index) => (
               <CarouselItem key={index}>
                 <div>
-                  <img src='/hero.png'></img>
+                  <img
+                    style={{ width: 'clamp(22.56rem, 100vw, 75rem)' }}
+                    src='/hero.png'
+                  ></img>
                 </div>
               </CarouselItem>
             ))}
@@ -34,8 +54,8 @@ function Home() {
           <CarouselNext />
         </Carousel>
         {/* genres */}
-        <div className='flex justify-around gap-3'>
-          {genres.map((item, index) => (
+        <div className='flex  justify-around gap-3'>
+          {genres?.map((item, index) => (
             <div
               key={index}
               className='shadow-card my-12 bg-white rounded-xl w-full  flex flex-col p-3 gap-3'
@@ -49,9 +69,21 @@ function Home() {
         </div>
 
         {/* recomendation */}
-
         <h1>Recomendation</h1>
-        <Card className='md:w-1/5' />
+        <div className='flex pt-5 gap-5'>
+          {data.map((item: Book) => (
+            <Link key={item.id} to={`/detail/${item.id}`} className='md:w-1/5'>
+              <Card
+                className='w-full'
+                name={item.title}
+                author={item.author.name}
+                image={item.coverImage}
+                rating={item.rating}
+              />
+            </Link>
+          ))}
+        </div>
+
         <LoadMoreButton className='md:mt-10 md:mb-12' />
 
         <div className='border-t border-neutral-300'></div>
@@ -60,9 +92,9 @@ function Home() {
           Popular Authors
         </h1>
         {/* author card */}
-        <div className='md:w-1/4'>
-          <AuthorCard />
-        </div>
+        <Link to={'/authors-book/:id'}>
+          <PopularAuthor />
+        </Link>
       </div>
     </>
   );
