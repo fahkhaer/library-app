bc28409687c7cd58859a8f5a6a38b1f1c39807a4
fahkhaer <khaeranifah@gmail.com>
2025-11-21 09:57:50 +0700

    Naviagate Booklist

--- src/pages/BookList.tsx
+++ src/pages/BookList.tsx
@@ -2,17 +2,20 @@ import { Button } from '@/components/ui/button';
 import CardListUser from '@/components/ui/CardListUser';
 import { Command, CommandInput } from '@/components/ui/command';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Link } from 'react-router-dom';
 
 function BookList() {
   return (
     <section className='flex flex-col mt-6 gap-6 pb-[110px]'>
       <h1>Book List</h1>
-      <Button
-        variant='default'
-        className='bg-[#1C65DA] hover:bg-blue-900 text-neutral-25 w-60 rounded-full p-2'
-      >
-        <h3>Add Book </h3>{' '}
-      </Button>
+      <Link to={'/add-book'}>
+        <Button
+          variant='default'
+          className='bg-[#1C65DA] hover:bg-blue-900 text-neutral-25 w-60 rounded-full p-2'
+        >
+          <h3>Add Book </h3>{' '}
+        </Button>
+      </Link>
       {/* search bar */}
       <div className='w-[600px]'>
         <Command className='h-12 rounded-full justify-center border border-neutral-300 gap-2 px-4 md:min-w-[500px]'>
