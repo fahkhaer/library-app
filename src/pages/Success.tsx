92c60ce78d8ca437a5ab895b2f5ad2c01f607548
fahkhaer <khaeranifah@gmail.com>
2025-11-21 08:29:42 +0700

    create success page ui

---
+++ src/pages/Success.tsx
@@ -0,0 +1,43 @@
+import Container from '@/components/layout/Container';
+import { Button } from '@/components/ui/button';
+import { CheckIcon } from 'lucide-react';
+
+function Success() {
+  return (
+    <Container className='h-screen flex items-center'>
+      <div className='w-full mx-auto flex justify-center'>
+        <div className='space-y-8 flex flex-col items-center text-center'>
+          <div>
+            <div className='relative flex items-center justify-center'>
+              {/* Outer rings */}
+              <div className='absolute size-[143px] rounded-full border-[1.5px] border-gray-200'></div>
+              <div className='absolute size-[130px] rounded-full border-[1.5px] border-gray-200'></div>
+              <div className='absolute size-[117px] rounded-full border-[1.5px] border-gray-200'></div>
+
+              {/* Main blue circle */}
+              <div className='size-[83px] rounded-full bg-[#1C65DA] flex items-center justify-center'>
+                <CheckIcon className='text-white size-[55px]' />
+              </div>
+            </div>
+          </div>
+
+          <div className='space-y-2 pt-8'>
+            <p className='text-[38px] font-bold leading-9'>
+              Borrowing Successful!
+            </p>
+            <p className='text-lg font-semibold leading-8'>
+              Your book has been successfully borrowed. Please return it by
+              <span className='text-[#EE1D52]'> 31 August 2025</span>
+            </p>
+          </div>
+
+          <Button variant={'secondary'} style={{width: 'clamp(3rem, 23.83vw, 17.88rem)'}}>
+            <h3>See Borrowed List</h3>
+          </Button>
+        </div>
+      </div>
+    </Container>
+  );
+}
+
+export default Success;
