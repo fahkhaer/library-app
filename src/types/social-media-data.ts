afeaccb193bf34540de99187f53e2e771124b41a
fahkhaer <khaeranifah@gmail.com>
2025-11-21 10:52:50 +0700

    add types socialMediaData

---
+++ src/types/social-media-data.ts
@@ -0,0 +1,30 @@
+
+
+type Icon = {
+  href: string;
+  src: string; 
+  alt: string;
+};
+
+export const socialMediaData: Icon[] = [
+  {
+    href: 'https://www.facebook.com/',
+    src: '/icons/icon-facebook.svg',
+    alt: 'Facebook',
+  },
+  {
+    href: 'https://www.instagram.com/',
+    src: '/icons/icon-instagram.svg',
+    alt: 'Instagram',
+  },
+  {
+    href: 'https://www.linkedin.com/',
+    src: '/icons/icon-linkedin.svg',
+    alt: 'LinkedIn',
+  },
+  {
+    href: 'https://www.tiktok.com/',
+    src: '/icons/icon-tiktok.svg',
+    alt: 'Tiktok',
+  },
+];
