78d0ac2496a1e012cf21f9c8e40942bf4655cb66
fahkhaer <khaeranifah@gmail.com>
2025-11-21 08:33:45 +0700

    api show profile user

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,17 @@
-import { AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Avatar } from '@radix-ui/react-avatar';
+import { useFetchUser } from '@/api/auth';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 function Profile() {
+  const { data: user, isLoading } = useFetchUser();
+
+  console.log('ini', user);
+  if (isLoading || !user) return <p> Loading...</p>;
+
   return (
     <section className='flex flex-col mt-6 gap-6 pb-[110px]'>
-      <h1 className=''>Profile</h1>
-      <div className=' shadow-card p-5 md:w-[557px] rounded-2xl  bg-white'>
-        {/* identity */}
+      <h1>Profile</h1>
+      <div className='shadow-card p-5 md:w-[557px] rounded-2xl bg-white'>
         <div className='space-y-3'>
           <Avatar>
             <AvatarImage
@@ -15,20 +19,24 @@ function Profile() {
               src='https://github.com/shadcn.png'
             />
             <AvatarFallback>CN</AvatarFallback>
-          </Avatar>{' '}
+          </Avatar>
+
           <div className='flex justify-between'>
             <h4>Name</h4>
-            <h3>JohnDoe</h3>
+            <h3>{user.name}</h3>
           </div>
+
           <div className='flex justify-between'>
             <h4>Email</h4>
-            <h3>johndoe@email.com</h3>
+            <h3>{user.email}</h3>
           </div>
+
           <div className='flex justify-between'>
             <h4>Nomor Handphone</h4>
-            <h3>081234567890s</h3>
+            <h3>{user.phone}</h3>
           </div>
-          <Button variant={'secondary'} className='w-full'>
+
+          <Button variant='secondary' className='w-full'>
             Update Profile
           </Button>
         </div>
