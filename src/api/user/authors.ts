85946361c1bee7662d19457880da06c6f157e2bf
fahkhaer <khaeranifah@gmail.com>
2025-11-22 11:50:12 +0700

    auth

---
+++ src/api/user/authors.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { baseUrl } from './booklist';
+
+export function GetAuthors() {
+  return useQuery({
+    queryKey: ['authors'],
+    queryFn: async () => {
+      const res = await axios.get(`${baseUrl}/api/authors`);
+      return res.data.data.authors;
+    },
+  });
+}
+
+export function BooksByAuthor(id: number) {
+  return useQuery({
+    queryKey: ['booksbyauthor', id],
+    queryFn: async () => {
+      const res = await axios.get(`${baseUrl}/api/authors/${id}/books`);
+      return res.data.data;
+    },
+  });
+}
