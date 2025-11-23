989745c82deb56d527e5c65d05a19f2851242361
fahkhaer <khaeranifah@gmail.com>
2025-11-23 07:23:38 +0700

    add props cardlist

--- src/components/ui/CardList.tsx
+++ src/components/ui/CardList.tsx
@@ -1,7 +1,14 @@
 import { Badge } from './badge';
 import { Star } from 'lucide-react';
+type CardListProps = {
+  genre?: string;
+  title?: string;
+  author?: string;
+  rating?: number;
+  image?: string | null;
+};
 
-function CardList() {
+function CardList({ genre, title, author, rating, }: CardListProps) {
   return (
     <div className='flex justify-end p-0 rounded-2xl  items-center space-y-4'>
       {/* left side */}
@@ -9,16 +16,16 @@ function CardList() {
         <img className='h-[138px] w-auto' src='/cover.png' alt='' />
         <div className='flex flex-col gap-1'>
           <Badge variant={'outline'} className='px-2 rounded-sm'>
-            Business & Economics{' '}
+            {genre}
           </Badge>
-          <h2>Book Name</h2>
-          <h4>Author Name</h4>
+          <h2>{title}</h2>
+          <h4>{author}</h4>
           <div className='flex gap-0.5 items-center'>
             <Star
               className='inline-block size-6 text-[#FFAB0D]'
               fill='#FFAB0D'
             />
-            <span className='text-md-semibold ml-1'>4.5</span>
+            <span className='text-md-semibold ml-1'>{rating}</span>
           </div>
         </div>
       </div>
