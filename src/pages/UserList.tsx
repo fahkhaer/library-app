0871e50ea17e5db33305690b1703cf9f6e01d097
fahkhaer <khaeranifah@gmail.com>
2025-11-25 16:09:23 +0700

    userslist.tsx get list user

--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -18,26 +18,36 @@ import {
   PaginationPrevious,
 } from '@/components/ui/pagination';
 import { ScrollArea } from '@/components/ui/scroll-area';
-
-const users = [
-  {
-    no: 1,
-    name: 'John Doe',
-    phone: '081234567890',
-    email: 'johndoe@email.com',
-    createdAt: '28 Aug 2025, 14:00',
-  },
-  {
-    no: 2,
-    name: 'Jane Smith',
-    phone: '081234567891',
-    email: 'janesmith@email.com',
-    createdAt: '29 Aug 2025, 10:30',
-  },
-  // Tambahkan data user lainnya
-];
+import { GetUsers, GetUsersPage } from '@/api/admin/user';
+import dayjs from 'dayjs';
+import { AdminUser } from '@/types/admin';
+import { useState } from 'react';
 
 function UserList() {
+  const {
+    data: users,
+    isLoading: loadingUsers,
+    error: errorUsers,
+  } = GetUsers();
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const {
+    data: pages,
+    isLoading: loadingPages,
+    error: errorPages,
+  } = GetUsersPage();
+
+  const pagedUsers: AdminUser[] = pages?.users || [];
+  const totalPages = pages?.totalPages || 1;
+  const totalEntries = pages?.total || 0;
+  const limit = pages?.limit || 10;
+
+  if (loadingUsers || loadingPages) return <p>Loading...</p>;
+  if (errorUsers || errorPages) return <p>Error loading users</p>;
+
+  console.log(pages);
+
   return (
     <section className='flex flex-col mt-6 gap-6 pb-[110px]'>
       <h1>User</h1>
@@ -64,13 +74,15 @@ function UserList() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {users.map((user) => (
-              <TableRow key={user.no}>
-                <TableCell className='text-center'>{user.no}</TableCell>
+            {users.map((user: AdminUser, i: number) => (
+              <TableRow key={user.id}>
+                <TableCell className='text-center'>{i + 1}</TableCell>
                 <TableCell>{user.name}</TableCell>
-                <TableCell>{user.phone}</TableCell>
+                <TableCell>{user.phone || '-'}</TableCell>
                 <TableCell>{user.email}</TableCell>
-                <TableCell>{user.createdAt}</TableCell>
+                <TableCell>
+                  {dayjs(user.createdAt).format('DD MMM YYYY, HH:mm')}
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
@@ -79,12 +91,20 @@ function UserList() {
             <TableRow>
               <TableCell colSpan={5} className='p-0'>
                 <div className='flex justify-between items-center px-4 py-4 text-sm text-neutral-700 bg-white'>
-                  <p>Showing 1 to 10 of 60 entries</p>
+                  <p>
+                    Showing {(currentPage - 1) * limit + 1} to{' '}
+                    {Math.min(currentPage * limit, totalEntries)} of{' '}
+                    {totalEntries} entries
+                  </p>
                   <div className='flex items-center space-x-1'>
                     <Pagination>
                       <PaginationContent>
                         <PaginationItem>
-                          <PaginationPrevious href='#' />
+                          <PaginationPrevious
+                            onClick={() =>
+                              setCurrentPage(Math.max(1, currentPage - 1))
+                            }
+                          />
                         </PaginationItem>
                         <PaginationItem>
                           <PaginationLink href='#'>1</PaginationLink>
@@ -94,6 +114,7 @@ function UserList() {
                             2
                           </PaginationLink>
                         </PaginationItem>
+
                         <PaginationItem>
                           <PaginationLink href='#'>3</PaginationLink>
                         </PaginationItem>
@@ -113,15 +134,18 @@ function UserList() {
         </Table>
       </div>
 
-      {/* Mobile Table as ScrollArea */}
       <div className='md:hidden'>
+        {/* Mobile Table as ScrollArea */}{' '}
         <ScrollArea className='w-full h-[400px]'>
+          {' '}
           <div className=''>
+            {' '}
             {users.map((user) => (
               <div
                 key={user.no}
                 className='bg-white shadow-card my-5 rounded-xl gap-5 p-4 text-sm-semibold leading-[28px]'
               >
+                {' '}
                 <div>
                   <span className='font-semibold'>No:</span> {user.no}
                 </div>
@@ -129,6 +153,7 @@ function UserList() {
                   <span className='font-semibold'>Name:</span> {user.name}
                 </div>
                 <div>
+                  {' '}
                   <span className='font-semibold'>Nomor Handphone:</span>{' '}
                   {user.phone}
                 </div>
@@ -136,20 +161,22 @@ function UserList() {
                   <span className='font-semibold'>Email:</span> {user.email}
                 </div>
                 <div>
+                  {' '}
                   <span className='font-semibold'>Created At:</span>{' '}
                   {user.createdAt}
                 </div>
               </div>
-            ))}
-          </div>
+            ))}{' '}
+          </div>{' '}
         </ScrollArea>
-
-        {/* Mobile Pagination */}
-        <div className='mt-4 px-2'>
+        {/* Mobile Table as ScrollArea */}
+        <div className='md:hidden mt-4 px-2'>
           <Pagination>
             <PaginationContent>
               <PaginationItem>
-                <PaginationPrevious href='#' />
+                <PaginationPrevious
+                  onClick={() => setCurrentPage(Math.max(1, currentPage - 1))}
+                />
               </PaginationItem>
               <PaginationItem>
                 <PaginationLink href='#'>1</PaginationLink>
