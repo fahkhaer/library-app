d01b27cf66bb7abca3bda2bad9de2be4ad8ab533
fahkhaer <khaeranifah@gmail.com>
2025-11-25 17:52:37 +0700

    map items on cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,17 @@
+import { GetCart } from '@/api/user/cart';
 import Container from '@/components/layout/Container';
 import { Button } from '@/components/ui/button';
 import CardList from '@/components/ui/CardList';
 import { Checkbox } from '@/components/ui/checkbox';
+import { OrderItem } from '@/types/cart';
 import { Link } from 'react-router-dom';
 
 function Cart() {
+  const { data, isLoading, error } = GetCart();
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading cart</p>;
+
   return (
     <Container className='pb-[100px]'>
       <h1 className='pb-8'>My Cart</h1>
@@ -21,15 +28,20 @@ function Cart() {
           {/* cards */}
           <div className='divide-y first:pt-0 flex flex-col divide-neutral-300 '>
             {/* card 1 */}
-            <div className='flex py-6 w-full gap-4 '>
-              <Checkbox id='select-all' />
-              <CardList />
-            </div>
-            {/* card 2  */}
-            <div className='flex py-6 w-full gap-4 '>
-              <Checkbox id='select-all' />
-              <CardList />
-            </div>
+            {data.items.length === 0 ? (
+              <p>Cart empty</p>
+            ) : (
+              data?.items?.map((item: OrderItem, i: number) => (
+                <div key={i} className='flex py-6 w-full gap-4 '>
+                  <Checkbox id='select-all' />
+                  <CardList
+                    bookId={item?.bookId}
+                    title={item?.book?.title}
+                    image={item?.book?.coverImage}
+                  />
+                </div>
+              ))
+            )}
           </div>
         </div>
         {/* right desktop*/}
@@ -37,7 +49,7 @@ function Cart() {
           <p className='text-xl font-bold'>Loan Summary</p>
           <div className='flex justify-between'>
             <h4>Total Book</h4>
-            <h3>2 Items</h3>
+            <h3>{data.items.length} Items</h3>
           </div>
           <Button className='w-full' variant={'secondary'}>
             <Link to={'/checkout'}>Borrow Books</Link>
