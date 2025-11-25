dc493b727537ac80858756759ef28759230cfe89
fahkhaer <khaeranifah@gmail.com>
2025-11-25 16:09:35 +0700

    type of user

---
+++ src/api/admin/user.ts
@@ -0,0 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { baseUrl } from '../user/booklist';
+
+export function GetUsers() {
+  const token = localStorage.getItem('token');
+
+  return useQuery({
+    queryKey: ['users'],
+    queryFn: async () => {
+      const res = await axios.get(`${baseUrl}/api/admin/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      return res.data.data.users;
+    },
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+  });
+}
+
+export function GetUsersPage() {
+  const token = localStorage.getItem('token');
+
+  return useQuery({
+    queryKey: ['userspage'],
+    queryFn: async () => {
+      const res = await axios.get(`${baseUrl}/api/admin/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      console.log(res.data.data.pagination);
+      return res.data.data.pagination;
+    },
+    refetchOnWindowFocus: true,
+    refetchOnReconnect: true,
+  });
+}
