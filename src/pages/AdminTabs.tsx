977977671ecad941463e0047d7ef2002c9c78e7a
fahkhaer <khaeranifah@gmail.com>
2025-11-25 16:31:13 +0700

    admin tabs route

--- src/pages/AdminTabs.tsx
+++ src/pages/AdminTabs.tsx
@@ -2,12 +2,23 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import BookList from './BookList';
 import Borrowers from './Borrowers';
 import UserList from './UserList';
+import { useSearchParams } from 'react-router-dom';
 
-function AdminTabs() {
+interface AdminTabsProps {
+  defaultTab?: 'borrowers' | 'userlist' | 'booklist';
+}
+
+function AdminTabs({ defaultTab = 'borrowers' }: AdminTabsProps) {
+  const [searchParams] = useSearchParams();
+  const tabFromQuery = searchParams.get('tab') as
+    | 'borrowers'
+    | 'userlist'
+    | 'booklist'
+    | null;
   return (
     <section className='pt-12 pb-[45px]'>
       {/* tabs */}
-      <Tabs defaultValue='booklist' className=' rounded-xl '>
+      <Tabs defaultValue={tabFromQuery || defaultTab} className=' rounded-xl '>
         <TabsList className='bg-[#F5F5F5]'>
           <TabsTrigger value='borrowers'>Borrowed List</TabsTrigger>
           <TabsTrigger value='userlist'>User</TabsTrigger>
