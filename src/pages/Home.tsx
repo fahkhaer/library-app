dbab42b241a295edffbd0391fc344ec20865498a
fahkhaer <khaeranifah@gmail.com>
2025-11-25 17:55:06 +0700

    add basis in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -80,11 +80,11 @@ function Home() {
           <Link
             key={item.id}
             to={`/detail/${item.id}`}
-            className='flex-1 min-w-[9rem] max-w-[16rem]'
+            className='flex flex-col basis-[calc(50%-0.25rem)] md:basis-[calc(25%-0.9375rem)]'
           >
             <Card
               name={item.title}
-              author={item.author.name}
+              author={item.Author.name}
               image={item.coverImage}
               rating={item.rating}
             />
