08414ff0cb2217591e8c240b8b397f9ac31f7829
fahkhaer <khaeranifah@gmail.com>
2025-11-26 16:16:10 +0700

    turn on searchbar in reviews page

--- src/pages/Reviews.tsx
+++ src/pages/Reviews.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { GetMyReview } from '@/api/user/reviews';
 import CardListBorrowed from '@/components/ui/CardListBorrowed';
 import { Command, CommandInput } from '@/components/ui/command';
@@ -7,8 +8,13 @@ import { Star } from 'lucide-react';
 
 function Reviews() {
   const { data: reviews, isLoading } = GetMyReview();
+  const [searchTerm, setSearchTerm] = useState('');
 
   if (isLoading) return <p> Loading...</p>;
+
+  const filteredReviews = reviews?.reviews?.filter((review: Review) =>
+    review.Book.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <section className='flex flex-col mt-4 md:mt-6 gap-6 md:pb-[110px]'>
@@ -20,45 +26,52 @@ function Reviews() {
           <CommandInput
             className='text-neutral-600 text-sm '
             placeholder='Search book '
+            onValueChange={(value) => setSearchTerm(value)}
           />
         </Command>
       </div>
-      {/* tabs and content */}
 
-      {reviews.reviews.map((review: Review) => (
-        <div
-          key={review.id}
-          className='shadow-card divide-neutral-300 divide-y bg-white rounded-2xl p-5 space-y-5'
-        >
-          <p className='text-sm-semibold md:text-md-semibold'>
-            {dayjs(review.createdAt).format('DD MMM YYYY, h:mm')}
-          </p>
-          <div className='pt-5'>
-            <CardListBorrowed
-              bookId={review?.bookId}
-              title={review.Book.title}
-              rating={review.star}
-              image={review.Book.coverImage || '/cover-off.png'}
-            />
-          </div>
-          <div className='flex flex-col gap-2 pt-5'>
-            <div className='flex'>
-              {Array.from({ length: 5 }).map((_, i) => (
-                <Star
-                  key={i}
-                  className='inline-block size-6 border-none text-[#FFAB0D]'
-                  stroke={i < review.star ? '#FFAB0D' : '#A4A7AE'}
-                  fill={i < review.star ? '#FFAB0D' : '#A4A7AE'}
-                />
-              ))}
-            </div>
-            {/* comment */}
+      {/* content */}
+      {filteredReviews.length === 0 ? (
+        <p className='text-neutral-500'>No matching reviews found.</p>
+      ) : (
+        filteredReviews.map((review: Review) => (
+          <div
+            key={review.id}
+            className='shadow-card divide-neutral-300 divide-y bg-white rounded-2xl p-5 space-y-5'
+          >
             <p className='text-sm-semibold md:text-md-semibold'>
-              {review.comment}
+              {dayjs(review.createdAt).format('DD MMM YYYY, h:mm')}
             </p>
+
+            <div className='pt-5'>
+              <CardListBorrowed
+                bookId={review?.bookId}
+                title={review.Book.title}
+                rating={review.star}
+                image={review.Book.coverImage || '/cover-off.png'}
+              />
+            </div>
+
+            <div className='flex flex-col gap-2 pt-5'>
+              <div className='flex'>
+                {Array.from({ length: 5 }).map((_, i) => (
+                  <Star
+                    key={i}
+                    className='inline-block size-6 border-none text-[#FFAB0D]'
+                    stroke={i < review.star ? '#FFAB0D' : '#A4A7AE'}
+                    fill={i < review.star ? '#FFAB0D' : '#A4A7AE'}
+                  />
+                ))}
+              </div>
+
+              <p className='text-sm-semibold md:text-md-semibold'>
+                {review.comment}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </section>
   );
 }
