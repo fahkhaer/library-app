bdc8d2f0f7fa1c31c2074e4d886ab04734d8fc80
fahkhaer <khaeranifah@gmail.com>
2025-11-30 12:49:18 +0700

    update loans type

--- src/types/loans.ts
+++ src/types/loans.ts
@@ -2,13 +2,22 @@ export type Loan = {
   id: number;
   userId: number;
   bookId: number;
-  status: 'BORROWED' | 'RETURNED';
+  status: 'BORROWED' | 'RETURNED' | 'LATE';
   borrowedAt: string;
   dueAt: string;
   returnedAt: string | null;
-  Book: {
+  Book?: {
     id: number;
     title: string;
     coverImage: string;
+    Author?: {
+      id: number;
+      name: string;
+    };
+  };
+  User?: {
+    id: number;
+    name: string;
+    email: string;
   };
 };
